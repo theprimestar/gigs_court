c0d1d149262f9235ebcb6c0f3b53479d4ccb9259
Gigscourt <theprimestarventures@gmail.com>
2026-05-08 19:56:46 +0100

    Create config.dart

---
+++ app/lib/config.dart
@@ -0,0 +1,4 @@
+class AppConfig {
+  static const String supabaseUrl = 'https://paohowngwtpffbdxxije.supabase.co';
+  static const String supabaseAnonKey = 'sb_publishable_sBrBW0vl1scg2ACsErDfag_IgIJj50Q';
+}
