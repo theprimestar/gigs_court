2dde4255a5d6b03400e5effb292ae68da8307c22
Gigscourt <theprimestarventures@gmail.com>
2026-05-09 07:38:24 +0100

    Update setup_step_one.dart

--- app/lib/screens/setup_step_one.dart
+++ app/lib/screens/setup_step_one.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
-import '../widgets/loading_dots.dart';
 
 class SetupStepOne extends StatefulWidget {
   final Map<String, dynamic> initialData;
