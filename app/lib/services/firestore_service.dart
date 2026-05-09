0fc19f54ba7e3ad21b3ea1bfe931f4ad50b1995a
Gigscourt <theprimestarventures@gmail.com>
2026-05-09 11:24:05 +0100

    Create firestore_service.dart

---
+++ app/lib/services/firestore_service.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirestoreService {
+  final _firestore = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+
+  Future<void> createProfile({
+    required String fullName,
+    required String photoUrl,
+    required String phone,
+    required String bio,
+    required List<String> services,
+  }) async {
+    final uid = _auth.currentUser!.uid;
+
+    await _firestore.collection('profiles').doc(uid).set({
+      'fullName': fullName,
+      'photoUrl': photoUrl,
+      'phone': phone,
+      'bio': bio,
+      'services': services,
+      'credits': 5,
+      'gigCount': 0,
+      'rating': 0.0,
+      'reviewCount': 0,
+      'createdAt': FieldValue.serverTimestamp(),
+      'updatedAt': FieldValue.serverTimestamp(),
+    });
+  }
+}
