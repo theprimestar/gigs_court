4051949295a35a815471abab84fc7921b595f403
Gigscourt <theprimestarventures@gmail.com>
2026-05-10 08:27:18 +0100

    Create provider_card_data.dart

---
+++ app/lib/models/provider_card_data.dart
@@ -0,0 +1,23 @@
+class ProviderCardData {
+  final String uid;
+  final String fullName;
+  final String photoUrl;
+  final double rating;
+  final int reviewCount;
+  final List<String> services;
+  final double distanceMeters;
+  final int gigCountThisMonth;
+  final int gigCountTotal;
+
+  const ProviderCardData({
+    required this.uid,
+    required this.fullName,
+    required this.photoUrl,
+    required this.rating,
+    required this.reviewCount,
+    required this.services,
+    required this.distanceMeters,
+    required this.gigCountThisMonth,
+    required this.gigCountTotal,
+  });
+}
