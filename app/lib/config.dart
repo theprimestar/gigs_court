4ad2c35f40a8aa4694687e86bc1ff2a6f1175776
Gigscourt <theprimestarventures@gmail.com>
2026-05-11 14:11:41 +0100

    Update config.dart

--- app/lib/config.dart
+++ app/lib/config.dart
@@ -4,4 +4,6 @@ class AppConfig {
   static const String imagekitPublicKey = 'public_hwM9hldZI+DqFY/pncPQCA5VRWo=';
   static const String imagekitUrlEndpoint = 'https://ik.imagekit.io/Theprimestar';
   static const String imagekitAuthEndpoint = 'https://paohowngwtpffbdxxije.supabase.co/functions/v1/imagekit-auth';
+  static const String paystackPublicKey = 'pk_test_4f6ae42964ab8da60e2f1c77cfb6fe1cd30806cc';
+  static const String paystackInitializeUrl = 'https://paohowngwtpffbdxxije.supabase.co/functions/v1/paystack-initialize';
 }
