385ea4535a8d07e2d6780b2ea15e437ed8c05753
Gigscourt <theprimestarventures@gmail.com>
2026-05-11 18:51:48 +0100

    Update profile_sheets.dart

--- app/lib/screens/profile_sheets.dart
+++ app/lib/screens/profile_sheets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_paystack_plus/flutter_paystack_plus.dart';
 import '../services/credit_service.dart';
 import '../services/chat_service.dart';
 import '../widgets/loading_dots.dart';
